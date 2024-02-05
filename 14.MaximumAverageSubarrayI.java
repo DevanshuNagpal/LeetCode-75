00a79791ff1318cc036782424e9e3096f20ff76e
Devanshu <133241751+DevanshuNagpal@users.noreply.github.com>
2024-02-05 11:28:20 +0530

    643. Maximum Average Subarray I

---
+++ 14.MaximumAverageSubarrayI.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        if(nums.length<k){return -1;}
+        int avg = 0;
+        int maxSum = 0;
+        for(int i = 0; i<k;i++){
+            maxSum += nums[i]; 
+        }
+        int windowSum = maxSum;
+        for(int i =k;i<nums.length;i++){
+            windowSum += nums[i] - nums[i-k];
+            maxSum = Math.max(windowSum , maxSum);
+
+        }
+        return (double)maxSum / k;
+    }
+}
