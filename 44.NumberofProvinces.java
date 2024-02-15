456c9807ce90a23747840df2f109fb011a50c6e2
Devanshu <133241751+DevanshuNagpal@users.noreply.github.com>
2024-02-15 21:23:20 +0530

    547. Number of Provinces

--- 44.NumberofProvinces.java
+++ 44.NumberofProvinces.java
@@ -1,31 +1,46 @@
-class Solution {
-    int n ;
-     public void dfs(int[][] isConnected, int u, ArrayList<Boolean> visited) {
-        visited.set(u, true);
-        // visit n neighubours
-        // find neightbours
-    
-        for (int v = 0 ;v<n;v++) {
-            if (!visited.get(v) && isConnected[u][v] == 1) {
-                dfs(isConnected, v, visited);
-            }
-        }
-    }
-
-     public int findCircleNum(int[][] isConnected) {
-         n = isConnected.length;
-        ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(n, false));
+    class Solution {
+        int n ;
+        public void dfs(int[][] isConnected, int u, ArrayList<Boolean> visited) {
+            visited.set(u, true);
+            // visit n neighubours
+            // find neightbours
         
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (!visited.get(i)) {
-                count++;
-
-                dfs(isConnected, i, visited);
+            for (int v = 0 ;v<n;v++) {
+                if (!visited.get(v) && isConnected[u][v] == 1) {
+                    dfs(isConnected, v, visited);
+                }
+            }
+        }
+        public void bfs(int[][] isConnected, int u, ArrayList<Boolean> visited) {
+            Queue<Integer> que = new LinkedList<>();
+            que.offer(u);
+            visited.set(u,true);
+            while(!que.isEmpty()){
+                int curr = que.poll();
+                for(int v = 0;v<n;v++){
+                    if (!visited.get(v) && isConnected[u][v] == 1) {
+                    bfs(isConnected, v, visited);
+                }
+                }
             }
         }
 
+        
 
-        return count;
+        public int findCircleNum(int[][] isConnected) {
+            n = isConnected.length;
+            ArrayList<Boolean> visited = new ArrayList<>(Collections.nCopies(n, false));
+            
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                if (!visited.get(i)) {
+                    count++;
+
+                    dfs(isConnected, i, visited);
+                }
+            }
+
+
+            return count;
+        }
     }
-}
